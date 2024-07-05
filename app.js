4f189d5c90aefd14b2a992ba02cc68c301dd3f18
Aerokinesis <alexwduong@gmail.com>
2024-07-04 22:36:14 -1000

    Added logic to the app and interactivity

--- app.js
+++ app.js
@@ -26,6 +26,32 @@ function drawGrid(size = 16) {
     }
 }
 
+gridSize.addEventListener("click", function() {
+    userSelectedSize = parseInt(prompt("Enter a grid size"));
+
+    if (userSelectedSize > 100) {
+        alert("Too big");
+    } else if (userSelectedSize <= 0) {
+        alert("Enter a positive number greater than 0.");
+    } else if (Number.isNaN(userSelectedSize)) {
+        alert("Not a number")
+    } else {
+        console.log(userSelectedSize);
+
+        container.innerHTML = "";
+        drawGrid(userSelectedSize);
+    }
+
+    
+})
+
+clearBoard.addEventListener("click", function() {
+    container.innerHTML = "";
+    drawGrid();
+    console.log("cleared");
+})
+
+
 console.log(container);
 console.log(drawGrid());
 
